e6c0f1a1912fc6688bcf9cf1d81b596a93c860d8
yanshan0707 <yanshan0707@126.com>
2015-08-23 21:25:17 +0800

    finished

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,9 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("please input the three sides a,b,c");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int s = (a + b + c) / 2;
+            double area=0;
+            if(a+b>c&&a+c>b&&b+c>a)
+            {
+                area= Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+                Console.WriteLine("the area is {0}", area);
+            }
+            else
+            {
+                Console.WriteLine("It is not a triangle");
+            }
             
         }
     }
--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("please input a three-digit integer");
+            int a = Convert.ToInt32(Console.ReadLine());
+            bool arms = Arms(a);
+            if(arms==true)
+                Console.WriteLine("It is an Armstrong number!");
+            else
+                Console.WriteLine("It isn't an Armstrong number!");
+
         }
-    }
+        static bool Arms(int a)
+        {
+            double b = Math.Pow(a / 100, 3);
+            double c = Math.Pow(a%100/10,3);
+            double d = Math.Pow(a%10, 3);
+            bool arms = false;
+            if (b + c + d == a)
+                arms = true;
+            return arms;
+        }
+    }   
+ 
+
+
 }
